21c5bd61eda06860378c7b7e143ea1de21769c02
Thaisrs165 <thaisrs3@al.insper.edu.br>
2025-11-16 14:49:36 -0300

    .

---
+++ CleanLang/out.asm
@@ -0,0 +1,9 @@
+PRINT Iniciando teste de sujeira...\n
+WALK
+TURN
+PRINT Sala parcialmente limpa...\n
+WALK
+PRINT Cozinha com sujeira ainda...\n
+PRINT Voltando pra base...\n
+WALK
+PRINT Fim do teste.\n
